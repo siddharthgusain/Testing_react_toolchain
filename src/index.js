431bdb0bd3178de788b12de861ba5f9be283ccb1
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-28 03:26:11 +0530

    where is webpack?

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ root.render(
   <App />
 );
 
+/*
+NOTE:-We Can't see webpack in this project package.json as it is dependency of "react-script package" and is present 
+in its package.json , while npm resolves the package.json recursively , it creates this dependency graph
+*/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
